841e42263e944dc1cfb64bec1a33c57ed50bc9d8
NDedov <ndedov@gmail.com>
2022-05-15 13:53:09 +0700

    commit_lesson 5

--- app/src/main/java/com/example/calcstart/MainActivity.java
+++ app/src/main/java/com/example/calcstart/MainActivity.java
@@ -12,12 +12,29 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    private final String KEY_APPLICATION = "KEY_APPLICATION";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button runCalc = findViewById(R.id.button);
+
+        runCalc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("example://intent");
+                Intent runCalcIntent = new Intent(Intent.ACTION_VIEW, uri);
+                runCalcIntent.putExtra(KEY_APPLICATION, "Запускатель калькулятора");
+
+                @SuppressLint("WrongConstant") ActivityInfo activityInfo =
+                        runCalcIntent.resolveActivityInfo(getPackageManager(), runCalcIntent.getFlags());
+                if (activityInfo != null) {
+                    startActivity(runCalcIntent);
+                }
+
+            }
+        });
     }
 }
